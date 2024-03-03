72ba60c63702630a58ebb5316ff5e517c13a6a02
wenjie <wenjie.sab@gmail.com>
2024-03-03 22:26:33 +0100

    Finalizado!

--- base_de_datos.c
+++ base_de_datos.c
@@ -32,15 +32,15 @@ int main(int argc, char *argv[]) {
 	// --- Fin de la inicializacion ---
 	
 	// --- Creacion de la base de datos y de la tabla
-	mysql_query(cnx, "drop database if exists juego;");
-	err=mysql_query(cnx, "create database juego;");
+	mysql_query(cnx, "drop database if exists juego");
+	err=mysql_query(cnx, "create database juego");
 	if(err!= 0){
 		printf("Error al crear la base de datos: %u &s\n", mysql_errno(cnx),mysql_error(cnx));
 		exit(1);
 	}
 	
 	err=mysql_query(cnx, "use juego");
-	err=mysql_query(cnx, "create table Partida (JugadorA integer, JugadorB integer, chat_id integer);");
+	err=mysql_query(cnx, "create table Partida (JugadorA integer, JugadorB integer, chat_id integer)");
 	if(err!= 0){
 		printf("Error al crear la tabla: %u &s\n", mysql_errno(cnx),mysql_error(cnx));
 		exit(1);
@@ -49,9 +49,9 @@ int main(int argc, char *argv[]) {
 	// --- Fin de la creacion de la base de datos y de la tabla ---
 	
 	// --- Introducción de datos como ejemplo ---
-	err=mysql_query(cnx, "insert into Partida (JugadorA, JugadorB, chat_id) values (1,2,1);");
-	err=mysql_query(cnx, "insert into Partida (JugadorA, JugadorB, chat_id) values (2,3,2);");
-	err=mysql_query(cnx, "insert into Partida (JugadorA, JugadorB, chat_id) values (4,5,3);");
+	err=mysql_query(cnx, "insert into Partida (JugadorA, JugadorB, chat_id) values (1,2,1)");
+	err=mysql_query(cnx, "insert into Partida (JugadorA, JugadorB, chat_id) values (2,3,2)");
+	err=mysql_query(cnx, "insert into Partida (JugadorA, JugadorB, chat_id) values (4,5,3)");
 	if(err!= 0){
 		printf("Error al insertar datos: %u &s\n", mysql_errno(cnx),mysql_error(cnx));
 		exit(1);
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]) {
 	// --- Fin de la introducir datos ---
 	
 	// --- Consulta ---
-	err=mysql_query(cnx,"select * from Partida;");
+	err=mysql_query(cnx,"select * from Partida");
 	
 	resultado = mysql_store_result(cnx);
 	row = mysql_fetch_row(resultado);
@@ -70,9 +70,17 @@ int main(int argc, char *argv[]) {
 	}
 	else{
 		while(row !=NULL){
-			
+			JugadorA = atoi(row[0]);
+			JugadorB = atoi(row[1]);
+			chat_id = atoi(row[2]);
+			printf("Id del Jugador A: %d \n", JugadorA);
+			printf("Id del Jugador B: %d \n", JugadorB);
+			printf("Id del chat: %d \n", chat_id);
+			row = mysql_fetch_row(resultado);
 		}
 	}
+	mysql_close(cnx);
+	
 	
 	return 0;
 }
